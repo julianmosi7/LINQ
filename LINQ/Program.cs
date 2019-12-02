ca946473593e58872374f63a9c9870a33e070ae2
julianmosi7 <moshammer.ju@gmail.com>
2019-12-02 22:28:09 +0100

    1, 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -14,10 +14,42 @@ namespace LINQ
             
         }
 
-        public static List<string> AllNamesWith4OrMoreLetters()
+        public static string[] AllNamesWith4OrMoreLetters()
         {
             return names
-                .Where(x => x.Count < 4)
+                .Where(x => x.Length < 4)
+                .ToArray();
+        }
+
+        public static string[] AllNamesContainingA()
+        {
+            string[] n = names
+                .Where(x => x.Contains("a"))                
+                .ToArray();
+            foreach (var item in n)
+            {
+                item.ToUpper();
+            }
+            return n;
+        }
+
+        public static string[] AllNamesHavingMoreThan3Letters()
+        {
+            return names
+                .Where(x => (x.Length > 3) && ((x.ElementAt(x.Length) == 'i') || (x.ElementAt(x.Length) == 'I')))
+                .ToArray();
+        }
+
+        public static string[] SplitNamesInFirstAndLastName()
+        {
+            string[][] n = names
+                .Select(x => x.Split(" "))
+                .ToArray();
+            for (int i = 0; i < n.Length; i++)
+            {
+                n[i][0].ToUpper();
+            }
+            return n;
         }
     }
 }
